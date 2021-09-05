b7635b20abe5f46fe36701e9e670eab60e610dbc
Charly Delay <0xcharly@users.noreply.github.com>
2021-09-05 15:51:28 +0900

    Moves most of keymap/layout related code under /users/delay
    In addition to reducing duplication and greatly improving maintenance
    and shareability, this commit also fine tunes a number of parameters to
    improve overall typing performance and comfort.  Among many other
    things, this commit brings:
    
      * Better detection of Home Row Mode in `get_tapping_term_per_key(…)`
      * Tune `TAPPING_TERM` per key
      * Enable `HOLD_ON_OTHER_KEYPRESS` for layer keys (for faster layers)
      * Enable `PERMISSION_HOLD` for home row keys only (for home rows)
      * Improve layout shareability regardless of keyboard form-factor
      * Improve feature compartmentalization for compatibility with various
        keyboards
      * Add support for bootmagic config when detecting activated mods

--- users/delay/delay.h
+++ users/delay/delay.h
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2021 Charly Delay <charly@codesink.dev> @0xcharly
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #pragma once
 
 /**
@@ -5,10 +21,42 @@
  *
  * Use `SAFE_RANGE_KEYMAP` for keymap specific codes, instead of `SAFE_RANGE`.
  */
-enum delay_custom_keycodes {
+enum keycodes_user {
   COMPOSE = SAFE_RANGE,
+  // Custom version of these keycodes to add special effects.
+  FX_RESET,  // Changes LEDs color, if any, before entering reset mode.
+  // Custom version of these keycodes that can't be shifted.
+  NS_0,
+  NS_1,
+  NS_2,
+  NS_3,
+  NS_4,
+  NS_5,
+  NS_6,
+  NS_7,
+  NS_8,
+  NS_9,
+  NS_BSLASH,
+  NS_COMMA,
+  NS_DOT,
+  NS_GRAVE,
+  NS_LBRACKET,
+  NS_RBRACKET,
+  NS_SCOLON,
+  NS_SLASH,
   SAFE_RANGE_KEYMAP,
 };
+
+// Shorthands.
+#define FX_RST FX_RESET
+
+#define NS_BSLS NS_BSLASH
+#define NS_COMM NS_COMMA
+#define NS_GRV NS_GRAVE
+#define NS_LBRC NS_LBRACKET
+#define NS_RBRC NS_RBRACKET
+#define NS_SCLN NS_SCOLON
+#define NS_SLSH NS_SLASH
 
 #ifdef COMPOSE_ENABLE
 /**
@@ -49,3 +97,34 @@ void matrix_scan_keymap(void);
  * docs.qmk.fm/using-qmk/software-features/feature_userspace#customized-functions.
  */
 void keyboard_post_init_keymap(void);
+
+/**
+ * Similar to `_kb`, `_user`, and other variants, but for individual keymaps.
+ *
+ * `_user` is already defined to add common functions to all keymaps, therefore
+ * a new version of the `eeconfig_init_*` callback is necessary for each keymap
+ * to add their own.
+ *
+ * docs.qmk.fm/using-qmk/software-features/feature_userspace#customized-functions.
+ */
+void eeconfig_init_keymap(void);
+
+#ifdef RGB_MATRIX_ENABLE
+/**
+ * Apply default/startup RGB matrix values.
+ *
+ * This is to replace the missing `rgb_matrix_reload_from_eeprom()`.
+ */
+void rgb_matrix_reset_noeeprom(void);
+#endif  // RGB_MATRIX_ENABLE
+
+/**
+ * Similar to `_kb`, `_user`, and other variants, but for individual keymaps.
+ *
+ * `_user` is already defined to add common functions to all keymaps, therefore
+ * a new version of the `oneshot_locked_mods_changed_*` callback is necessary
+ * for each keymap to add their own.
+ *
+ * docs.qmk.fm/using-qmk/software-features/feature_userspace#customized-functions.
+ */
+void oneshot_locked_mods_changed_keymap(uint8_t mods);
