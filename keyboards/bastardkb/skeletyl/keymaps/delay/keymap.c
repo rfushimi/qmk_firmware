8dbc587ffb703442496016dc7d09e27986a39954
Charly Delay <0xcharly@users.noreply.github.com>
2021-10-10 20:41:06 +0900

    users/delay: add Pointer-dedicated layer

--- keyboards/bastardkb/skeletyl/keymaps/delay/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/delay/keymap.c
@@ -42,6 +42,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_NAV] = LAYOUT_skeletyl(LAYER_NAV_split_3x5_3),
   [LAYER_NUM] = LAYOUT_skeletyl(LAYER_NUM_split_3x5_3),
   [LAYER_SYM] = LAYOUT_skeletyl(LAYER_SYM_split_3x5_3),
+  [LAYER_PTR] = LAYOUT_skeletyl(LAYER_PTR_split_3x5_3),
+
+#ifndef KEYMAP_DEFAULT_LITE
   [LAYER_EXP] = LAYOUT_skeletyl(LAYER_EXP_split_3x5_3),
+#endif  // !KEYMAP_DEFAULT_LITE
 };
 // clang-format on
