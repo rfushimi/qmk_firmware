5335bceee61b445366fb1ee6eaae4e7eb9b2a119
Charly Delay <0xcharly@users.noreply.github.com>
2021-10-10 21:12:30 +0900

    bastardkb/charybdis_lite: add Charybdis Nano with RGB support

---
+++ keyboards/bastardkb/charybdis_lite/keymaps/delay/rules.mk
@@ -0,0 +1,5 @@
+KEYMAP_ENABLE = lite # Use "lite" keymap (from /users/delay), ie. w/o training layouts.
+MOUSEKEY_ENABLE = no # Keymap uses mouse keys.
+
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
