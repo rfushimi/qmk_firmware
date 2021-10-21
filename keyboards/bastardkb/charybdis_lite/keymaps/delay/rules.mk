f646a616fd1c1240c4ac37e4a09dfb3b98821768
Charly Delay <0xcharly@users.noreply.github.com>
2021-10-21 23:22:24 +0900

    bastardkb/charybdis_lite:delay: enable COMPOSE and TAP_DANCE features

--- keyboards/bastardkb/charybdis_lite/keymaps/delay/rules.mk
+++ keyboards/bastardkb/charybdis_lite/keymaps/delay/rules.mk
@@ -1,3 +1,11 @@
+# Space-saving settings.
+LTO_ENABLE = yes
+QMK_SETTINGS = no
+COMBO_ENABLE = no
+
+COMPOSE_ENABLE = yes   # Enable composition.
+TAP_DANCE_ENABLE = yes # Use tap-dance-based one-shot shift.
+
 KEYMAP_ENABLE = lite # Use "lite" keymap (from /users/delay), ie. w/o training layouts.
 MOUSEKEY_ENABLE = no # Keymap uses mouse keys.
 
