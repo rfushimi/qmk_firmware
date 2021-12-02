87f6712222eb9904f709ae6396cbb2fe7784e390
Charly Delay <0xcharly@users.noreply.github.com>
2021-12-02 17:25:04 +0900

    users/delay: update comment syntax

--- users/delay/compose.h
+++ users/delay/compose.h
@@ -18,18 +18,18 @@
 
 #include QMK_KEYBOARD_H
 
-/** Default compose timeout at 1000ms. */
+/** \brief Default compose timeout at 1000ms. */
 #ifndef COMPOSE_TERM
 #define COMPOSE_TERM 1000
 #endif  // COMPOSE_TERM
 
-/** The size of the compose queue.  Defauts to 2 keycodes. */
+/** \brief The size of the compose queue.  Defauts to 2 keycodes. */
 #ifndef COMPOSE_KEYCODE_QUEUE_SIZE
 #define COMPOSE_KEYCODE_QUEUE_SIZE 2
 #endif
 
 /**
- * The state required to handle composing.
+ * \brief The state required to handle composing.
  */
 typedef struct {
   uint16_t keycode_queue[COMPOSE_KEYCODE_QUEUE_SIZE];
@@ -39,14 +39,14 @@ typedef struct {
 } compose_state_t;
 
 /**
- * Initialize the compose engine.
+ * \brief Initialize the compose engine.
  *
  * Should be called in `keyboard_post_init_user()`.
  */
 void keyboard_post_init_compose(compose_state_t *state);
 
 /**
- * Process the input keycode.
+ * \brief Process the input keycode.
  *
  * Should be called in `process_record_user(uint16_t, keyrecord_t)`.
  */
@@ -54,14 +54,14 @@ bool process_record_compose(compose_state_t *state, uint16_t keycode,
                             keyrecord_t *record);
 
 /**
- * Trigger compose on timeout.
+ * \brief Trigger compose on timeout.
  *
  * Should be called in `matrix_scan_user()`.
  */
 void matrix_scan_compose(compose_state_t *state);
 
 /**
- * Create a sequence of keycode used for composition.
+ * \brief Create a sequence of keycode used for composition.
  *
  * This macro ensures that the array is of the correct size.
  */
@@ -69,7 +69,8 @@ void matrix_scan_compose(compose_state_t *state);
   ((uint16_t[COMPOSE_KEYCODE_QUEUE_SIZE]){__VA_ARGS__})
 
 /**
- * Whether the compose queue contains all of the specified keycodes, in order.
+ * \brief Whether the compose queue contains all of the specified keycodes, in
+ * order.
  *
  * `COMPOSE_SEQUENCE` can be used to create the second argument to this
  * function, eg.
@@ -82,6 +83,8 @@ bool compose_queue_equal(compose_state_t *state,
                          uint16_t keycodes[COMPOSE_KEYCODE_QUEUE_SIZE]);
 
 /**
+ * \brief User level callback.
+ *
  * Use this function to add support for more composition on a per-user/-keymap
  * basis.
  */
