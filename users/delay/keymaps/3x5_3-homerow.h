565952bbb583eafc497f267ae811ec478e61ce49
Charly Delay <0xcharly@users.noreply.github.com>
2021-12-02 17:25:04 +0900

    users/delay: update comment syntax

--- users/delay/keymaps/3x5_3-homerow.h
+++ users/delay/keymaps/3x5_3-homerow.h
@@ -65,7 +65,7 @@ enum tapdance_keymap {
 #endif  // TAP_DANCE_ENABLE
 
 /**
- * Similar to `_kb`, `_user`, and other variants, but for individual keymaps.
+ * \brief Similar to `_kb`, `_user`, and other variants, but for keymaps.
  *
  * `_user` is already defined to add common functions to all keymaps, therefore
  * a new version of the `layer_state_set_*` callback is necessary for each
@@ -104,7 +104,7 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
 
 // clang-format off
 /**
- * Add Home Row mod to a layout.
+ * \brief Add Home Row mod to a layout.
  *
  * Expects a 10-key per row layout.  Adds support for GACS (Gui, Alt, Ctl, Shift)
  * home row.  The layout passed in parameter must contain at least 20 keycodes.
@@ -124,20 +124,20 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
       __VA_ARGS__
 #define HOME_ROW_MOD_GACS(...) _HOME_ROW_MOD_GACS(__VA_ARGS__)
 
-/** Convenience row shorthands. */
+/* Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 #define ______________HOME_ROW_GACS_L______________ KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX
 #define ______________HOME_ROW_GACS_R______________ XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI
 #define ________________RESET_ROW_L________________   RESET, EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX
 #define ________________RESET_ROW_R________________ XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST,   RESET
 
-/** Dvorak layout (3 rows, 10 columns). */
+/** \brief Dvorak layout (3 rows, 10 columns). */
 #define LAYER_ALPHAS_DVORAK_3x10                                                              \
     KC_QUOT, NS_COMM,  NS_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, \
        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
     KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z
 
-/** Colemak-DHm layout (3 rows, 10 columns). */
+/** \brief Colemak-DHm layout (3 rows, 10 columns). */
 #define LAYER_ALPHAS_COLEMAK_DHM_3x10                                                         \
        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y, NS_SCLN, \
        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O, \
@@ -148,25 +148,25 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
  * keys on the base layer (namely, underscore `_`).
  */
 
-/** Slightly modified Dvorak layout (3 rows, 10 columns). */
+/** \brief Slightly modified Dvorak layout (3 rows, 10 columns). */
 #define LAYER_ALPHAS_DVORAK_ALT_3x10                                                          \
     KC_QUOT, NS_COMM,  NS_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, \
        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
     KC_UNDS,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z
 
-/** Slightly modified Colemak-DHm layout (3 rows, 10 columns). */
+/** \brief Slightly modified Colemak-DHm layout (3 rows, 10 columns). */
 #define LAYER_ALPHAS_COLEMAK_DHM_ALT_3x10                                                     \
        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y, NS_UNDS, \
        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O, \
        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H, NS_COMM,  NS_DOT, KC_SLSH
 
-/** Thumb clusters used on split 3x5+3 keyboards. */
+/** \brief Thumb clusters used on split 3x5+3 keyboards. */
 #define LAYER_ALPHAS_THUMBS_1x6         TAB_DEV, SPC_NAV, OSM_SFT, ESC_SYM, ENT_NUM, OSL_EXP
 
-/** Thumb clusters used on split 3x5+3 keyboards. Use with homerow-free layouts.  */
+/** \brief Thumb clusters used on split 3x5+3 keyboards. Use with homerow-free layouts.  */
 #define LAYER_NO_MODS_ALPHAS_THUMBS_1x6    KC_TAB, KC_SPC, OSM_SFT, KC_ESC, ENT_NUM, OSL_EXP
 
-/**
+/*
  * Layers used on split 3x5+3 keyboards.
  *
  * These layers started off heavily inspired by the Miryoku layout, but trimmed
@@ -175,6 +175,8 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
  */
 
 /**
+ * \brief Development and Console layer.
+ *
  * The Development & Console layer contains symbols that are most commonly used
  * by programming languages, and shells.  Symbols are on the opposite hand.
  */
@@ -185,6 +187,8 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
                       _______, XXXXXXX, XXXXXXX,  FX_ESC,  KC_ENT, XXXXXXX
 
 /**
+ * \brief Navigation layer.
+ *
  * The Navigation layer contains the arrow keys on the opposite hand, and adds
  * the mouse buttons and cut/copy/paste fonctions on the same hand.
  */
@@ -195,6 +199,8 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
                       XXXXXXX, _______, XXXXXXX,  FX_ESC,  KC_ENT, XXXXXXX
 
 /**
+ * \brief Numerals and Symbols layer.
+ *
  * The Numerals & Symbols layer contains the numpad and common arithmetic
  * functions on the opposite hand.
  */
@@ -205,6 +211,8 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
                       KC_BSPC,    NS_0, KC_MINS, XXXXXXX, _______, XXXXXXX
 
 /**
+ * \brief Symbols layer.
+ *
  * The Symbol layer contains most symbols that didn't make the cut to the
  * Development & Console layer.  Those symbols are placed on the opposite hand.
  */
@@ -215,7 +223,9 @@ layer_state_t layer_state_set_keymap(layer_state_t state);
                        KC_TAB,  KC_SPC, KC_UNDS, _______, XXXXXXX, XXXXXXX
 
 /**
- * The Experiment layer serves as placeholder for new features currently under
+ * \brief Experiments layer.
+ *
+ * The Experiments layer serves as placeholder for new features currently under
  * active development, until they get promoted to one of the stable layers.
  */
 #define LAYER_EXP_split_3x5_3                                                                 \
