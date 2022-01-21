c773c4bf57b4a960842eafe5b4aaa169cdef6f91
Charly Delay <0xcharly@users.noreply.github.com>
2022-01-21 12:47:01 +0900

    users/delay: rationalize callback convention

--- users/delay/keymaps/split36.c
+++ users/delay/keymaps/split36.c
@@ -60,7 +60,10 @@ bool is_oneshot_mod_ignore_key(uint16_t keycode) {
 }
 #endif  // ONESHOT_MOD_ENABLE
 
-bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user_keymap(uint16_t keycode, keyrecord_t *record) {
+  if (!process_record_keymap(keycode, record)) {
+    return false;
+  }
   switch (keycode) {
     case CLEAR:
       clear_oneshot_mods();
@@ -87,6 +90,57 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 };
+
+__attribute__((weak)) bool process_record_keymap(uint16_t keycode,
+                                                 keyrecord_t *record) {
+  return true;
+}
+
+#ifdef RGB_MATRIX_ENABLE
+static uint16_t reset_rgb_matrix_timer = 0;
+#endif  // RGB_MATRIX_ENABLE
+
+void matrix_scan_user_keymap(void) {
+#ifdef RGB_MATRIX_ENABLE
+  if (reset_rgb_matrix_timer != 0 &&
+      TIMER_DIFF_16(timer_read(), reset_rgb_matrix_timer) >= 1000) {
+    rgb_matrix_reset_noeeprom();
+    reset_rgb_matrix_timer = 0;
+  }
+#endif  // RGB_MATRIX_ENABLE
+  matrix_scan_keymap();
+}
+
+__attribute__((weak)) void matrix_scan_keymap(void) {}
+
+/** Called on layer change. */
+layer_state_t layer_state_set_user_keymap(layer_state_t state) {
+#ifdef RGB_MATRIX_ENABLE
+  if (get_highest_layer(state) == _BASE) {
+    reset_rgb_matrix_timer = timer_read();
+  }
+#endif  // RGB_MATRIX_ENABLE
+  return layer_state_set_keymap(state);
+}
+
+__attribute__((weak)) layer_state_t layer_state_set_keymap(
+    layer_state_t state) {
+  return state;
+}
+
+void keyboard_post_init_user_keymap(void) { keyboard_post_init_keymap(); }
+
+__attribute__((weak)) void keyboard_post_init_keymap(void) {}
+
+void oneshot_locked_mods_changed_user_keymap(uint8_t mods) {
+  oneshot_locked_mods_changed_keymap(mods);
+}
+
+__attribute__((weak)) void oneshot_locked_mods_changed_keymap(uint8_t mods) {}
+
+void shutdown_user_keymap(void) { shutdown_keymap(); }
+
+__attribute__((weak)) void shutdown_keymap(void) {}
 
 #ifdef TAPPING_FORCE_HOLD_PER_KEY
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
