b8c338fa18070c0ceb2124e905002a83d456d46f
Charly Delay <0xcharly@users.noreply.github.com>
2022-01-23 19:32:42 +0900

    bastardkb/tbkmini:default: add Vial support

---
+++ keyboards/bastardkb/tbkmini/keymaps/default/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define EE_HANDS
+
+#define VIAL_KEYBOARD_UID \
+  { 0xF8, 0x43, 0x96, 0x57, 0x1E, 0x02, 0xF1, 0x56 }
+#define VIAL_UNLOCK_COMBO_ROWS \
+  { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS \
+  { 0, 0 }
+
+#define NO_ACTION_ONESHOT
+#define LAYER_STATE_8BIT
