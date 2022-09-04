a6b3ccdbe72aca14c45cd84fdff411d9e8568bc3
Charly Delay <0xcharly@codesink.dev>
2022-09-04 22:29:15 +0900

    Add drashna's code for sh1107 OLED

--- keyboards/bastardkb/dilemma/keymaps/delay/rules.mk
+++ keyboards/bastardkb/dilemma/keymaps/delay/rules.mk
@@ -1,3 +1,3 @@
 # Keymap supports OLED screen. Not enabled by default.
+OLED_DRIVER = custom
 OLED_SUPPORTED = yes
-OLED_ENABLE = no
--- users/delay/rules.mk
+++ users/delay/rules.mk
@@ -25,3 +25,13 @@ COMBO_ENABLE = yes
 DEFERRED_EXEC_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+
+# Drashna's OLED driver for SH1107.
+ifeq ($(strip $(OLED_DRIVER)), custom)
+ifeq ($(strip $(OLED_ENABLE)), yes)
+	OPT_DEFS += -DOLED_ENABLE -DOLED_DRIVER_SH1107
+	SRC += $(USER_PATH)/oled/sh110x.c
+	QUANTUM_LIB_SRC += i2c_master.c
+	DEFERRED_EXEC_ENABLE = yes
+endif
+endif
