f8e3b5607a9d1322b492fab96433a9a4dbc924cb
Charly <charly@delay.gg>
2022-09-19 23:55:31 +0900

    Add debug compile flag for Dilemma

--- keyboards/bastardkb/dilemma/keymaps/delay/rules.mk
+++ keyboards/bastardkb/dilemma/keymaps/delay/rules.mk
@@ -1,3 +1,8 @@
 # Keymap supports OLED screen. Not enabled by default.
 OLED_DRIVER = custom
 OLED_SUPPORTED = yes
+OLED_ENABLE = yes
+WPM_ENABLE = yes
+
+LTO_ENABLE = no
+EXTRAFLAGS += -Og -gdwarf-4
