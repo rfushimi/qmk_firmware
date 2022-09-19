dafabc6e168c743fcb395702f572b40a965edfdd
Charly <charly@delay.gg>
2022-09-19 23:53:36 +0900

    Add full-duplex to the RP2040 Skeletyl

--- keyboards/bastardkb/skeletyl/keymaps/delay/config.h
+++ keyboards/bastardkb/skeletyl/keymaps/delay/config.h
@@ -24,4 +24,10 @@
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 #    define SPLIT_HAND_PIN GP13
 #    define SPLIT_HAND_PIN_LOW_IS_LEFT // High -> right, Low -> left.
+
+#    undef SOFT_SERIAL_PIN
+#    define SERIAL_USART_FULL_DUPLEX // Enable full duplex operation mode.
+#    define SERIAL_USART_TX_PIN GP1  // USART TX pin
+#    define SERIAL_USART_RX_PIN GP12 // USART RX pin
+#    define SERIAL_USART_PIN_SWAP
 #endif
