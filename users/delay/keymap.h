726bba1306309f4db8c42b68caf70229e852501a
Charly Delay <charly@delay.gg>
2022-10-30 16:19:05 +0900

    Move btn3

--- users/delay/keymap.h
+++ users/delay/keymap.h
@@ -149,10 +149,10 @@ enum layers_keymap {
  */
 // clang-format off
 #define MOTION_split_3x5_2                                                                    \
-    LAUNCHR, RCS_TAB,  KC_TAB, CTL_TAB, ___x___, ___x___, KC_HOME,   KC_UP,  KC_END, ___x___, \
+    LAUNCHR, RCS_TAB,  KC_TAB, CTL_TAB, ___x___, ___x___, KC_HOME,   KC_UP,  KC_END, KC_BTN3, \
     OSM_ALT, OSM_GUI, OSM_CTL, OSM_SFT, ___x___, ___x___, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, \
     A(KC_1), A(KC_2), A(KC_3), A(KC_4), A(KC_5), A(KC_6), A(KC_7), A(KC_8), A(KC_9), A(KC_0), \
-                                SYSTEM, _______, _______, KC_BTN3
+                                SYSTEM, _______, _______, _______
 // clang-format on
 
 /**
