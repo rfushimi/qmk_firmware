46b48f6a90990b3e2d19498fbb63c5487c031190
Charly Delay <charly@delay.gg>
2022-11-10 00:59:41 +0900

    More adjustments to the 4x6 keymap

--- users/delay/keymap.h
+++ users/delay/keymap.h
@@ -223,6 +223,7 @@ enum layers_4x6_keymap {
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define RSE_BSP LT(_RAISE, KC_BACKSPACE)
 #define LOW_SPC LT(_LOWER, KC_SPACE)
 
 /**
@@ -231,10 +232,10 @@ enum layers_4x6_keymap {
 // clang-format off
 #define DVORAK_split_4x6_5                                                                                        \
      KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ESC,  \
-    KC_QUES,  KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH,  \
-     KC_TAB,     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_SCLN,  \
+     KC_TAB,  KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_BSLS,  \
+     KC_ESC,     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_SLSH,  \
     KC_LSFT,  KC_COLN,    KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,  \
-                                KC_LSFT,   RAISE, KC_LALT, KC_LGUI, LOW_SPC, ENT_CTL,                             \
+                                KC_LSFT, RSE_BSP, KC_LALT, KC_LGUI, LOW_SPC, ENT_CTL,                             \
                                          KC_RCTL, KC_LALT, KC_LGUI, KC_LCTL
 // clang-format on
 
