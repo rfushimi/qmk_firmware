73224e1c3d4dd277f8214def90c0001a31bb9f32
Charly Delay <delay@google.com>
2024-10-15 18:43:37 +0900

    hc-macropad: initial draft config

---
+++ keyboards/hc-macropad/hc-macropad.c
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2024 Charly Delay <0@0xcharly.com> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Publicw License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef DIP_SWITCH_ENABLE
+bool dip_switch_update_kb(uint8_t index, bool active) {
+    if (!dip_switch_update_user(index, active)) {
+        return false;
+    }
+    if (active) {
+        tap_code(KC_1);
+    }
+    return true;
+}
+#endif // DIP_SWITCH_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+// Layer state indicator
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) { return false; }
+    return false;
+};
+#endif // RGB_MATRIX_ENABLE
---
+++ keyboards/hc-macropad/readme.md
@@ -0,0 +1,5 @@
+# HC Macropad
+
+This keyboard is an custom edition for an internal event.
+
+The design is available at [0xcharly/hc-macropad](https://github.com/0xcharly/hc-macropad).
