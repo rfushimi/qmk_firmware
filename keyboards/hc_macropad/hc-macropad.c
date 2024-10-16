bcb5fc439adbfa03e7da3f38a6a941ec09b74a53
Charly Delay <delay@google.com>
2024-10-16 12:05:33 +0900

    hc-macropad: ensure that GP26 through GP29 are correctly configured as digital inputs (just in case we get an old batch of RP2040)

--- keyboards/hc_macropad/hc-macropad.c
+++ keyboards/hc_macropad/hc-macropad.c
@@ -15,6 +15,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+void keyboard_pre_init_kb(void) {
+    // Ensures that GP26 through GP29 are initialized as digital inputs (as
+    // opposed to analog inputs).  These GPIOs are shared with A0 through A3,
+    // respectively.  On RP2040-B2 and later, the digital inputs are disabled by
+    // default (see RP2040-E6).
+    gpio_init(GP26);
+    gpio_init(GP27);
+    gpio_init(GP28);
+    gpio_init(GP29);
+
+    keyboard_pre_init_user();
+}
+
 #ifdef DIP_SWITCH_ENABLE
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (!dip_switch_update_user(index, active)) {
